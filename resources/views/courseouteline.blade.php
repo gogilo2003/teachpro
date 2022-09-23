298c69a8deb7325731b1909b7be104e5470fe163
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2022-09-23 07:31:37 +0300

    Fri Sep 23 07:31:37 AM EAT 2022

--- resources/views/courseouteline.blade.php
+++ resources/views/courseouteline.blade.php
@@ -7,12 +7,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
   <body>
-  	<div class="container-fluid">
-    <h1>Course outline</h1>
-    <h2>Artisan 1</h2>
-    @foreach($artisan1 as $file)
-    	<img src="{{ $file }}"/>
-    @endforeach
-</div>
+  	<div class="container">
+        <h1>Course outline</h1>
+        <h2>Artisan 1</h2>
+        <div class="row">
+        @foreach($artisan1 as $file)
+        <div class="col-md-6 mb-3">
+        	<img src="{{ $file }}" style="width:100%"/>
+        </div>
+        <div class="col-md-6 mb-3">
+          <textarea class="form-control" style="width:100%; height: 100%"></textarea>
+        </div>
+        @endforeach
+      </div>
+    </div>
   </body>
 </html>
