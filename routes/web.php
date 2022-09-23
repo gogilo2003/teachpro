92023814ac36957467c1a11951105eeb3713e948
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2022-09-23 06:18:58 +0300

    Fri Sep 23 06:18:58 AM EAT 2022

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,7 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+    Route::get('/subjects', function () {
+        return Inertia::render('Subjects');
+    })->name('subjects');
 });
